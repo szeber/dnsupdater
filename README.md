ca6b2822344ff93a590afef294ed0832e10a9ead
Janos Pasztor <j.pasztor@ixolit.com>
2012-05-16 14:22:35 +0200

    Added update-ip script and readme

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
-dnsupdater
-==========
+Médiabirodalom DNS updater
+==========================
 
-DNS updater script a Médiabirodalom dinamikus DNS szolgáltatáshoz.
+DNS updater script a Médiabirodalom dinamikus DNS szolgáltatáshoz Linux/UNIX
+rendszerekre. Ez a script frissíti a Médiabirodalom DNS szervereket a
+mindenkori publikus IP-ddel.
+
+Feliratkozás
+------------
+
+A szolgáltatás használatához szükséged van egy Médiabirodalom felhasználónévre,
+amit a http://www.mediabirodalom.com címen regisztrálhatsz. Ezen felül szükséged
+lesz egy domain névre, ami alatt aldomaint tudsz létrehozni.
+
+Először is állítsd be a domain nevedet a szolgáltatód DNS szerverén például így:
+
+otthon.mintadomain.hu. IN NS ns1.dyn.iptool.eu
+otthon.mintadomain.hu. IN NS ns2.dyn.iptool.eu
+
+Ezek után generálj egy HMAC kulcsot. Ezt legegyszerűbben a dnssec-keygen
+utilityvel tudod megcsinálni:
+
+    dnssec-keygen -b 128 -a HMAC-MD5 -n USER userneved
+
+Ezek után a kulcsodat a Kuserneved.+157+valami.private fileban fogod találni:
+
+    Private-key-format: v1.3
+    Algorithm: 157 (HMAC_MD5)
+    Key: Z/RYvivchog5NHYjTqygOw==
+    Bits: AAA=
+    Created: 20120516120502
+    Publish: 20120516120502
+    Activate: 20120516120502
+
+Ezt a kulcsot a többi adatoddal együtt írd bele az update-ip.sh fileba. Ezek
+után írj egy e-mailt *a regisztrációkor megadott e-mail címedről* az
+ops@mediabirodalom.com címre a következő formátumban:
+
+    Tárgy: Dinamikus DNS igenyles
+
+    Teljes nev: A teljes neved
+    Felhasznalonev: userneved
+    Domain: otthon.mintadomain.hu
+    Kulcs: a kulcsod
+
+Fontos, hogy a Médiabirodalom a teljes polgári nevedet tartalmazza. A
+domainedhez tartozó beállítást 1-3 munkanapon belül csináljuk meg.
+
+Használat
+---------
+
+Az ip-update.sh scriptet 5 percenként futtasd cronból:
+
+    */5 * * * * /usr/local/mediabirodalom/update-ip.sh
---
+++ update-ip.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# A wget parancs eleresi utja.
+WGET=/usr/bin/wget
+
+# Az nsupdate parancs eleresi utja.
+NSUPDATE=/usr/bin/nsupdate
+
+# A domain nev, amit frissiteni kell.
+DOMAIN=
+
+# A Mediabirodalom userneved.
+KEYNAME=
+
+# A HMAC-MD5 kulcsod.
+KEY=
+
+# Az a file, amiben a rendszer tarolja a regi IP cimet.
+IPFILE=/var/local/mediabirodalom/lastip
+
+# A site, ahonnan a publikus IP cimet letolti a script.
+IPSITE=http://ip.iptool.eu/
+
+# A Mediabirodalom DNS szerver cime.
+DNSSERVER=dyn.iptool.eu
+
+###############################################################################
+# Innentol ne modosits                                                        +
+###############################################################################
+
+if [ ! -f $WGET ]; then
+	echo "wget is not installed." >&2
+	echo "This is required to determine the public IP address of your host." >&2
+	exit 1
+fi
+
+if [ ! -f $NSUPDATE ]; then
+	echo "nsupdate is not installed." >&2
+	echo "This is required to update the DNS records." >&2
+	exit 1
+fi
+
+PUBLICIP="$($WGET --quiet -O - $IPSITE 2>/dev/null)"
+if [ $? -ne 0 ]; then
+	echo "Failed to download public IP address from $IPSITE" >&2
+	exit 1
+fi
+
+if [[ !"$PUBLICIP" =~ "/^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/" ]]; then
+	echo "Invalid public IP address returned: $PUBLICIP" >&2
+	exit 1
+fi
+
+if [ -f "$IPFILE" ]; then
+	OLDIP=$(cat $IPFILE)
+	if [ "$OLDIP" == "$PUBLICIP" ]; then
+		# Same IP address, return silently
+		exit 0
+	fi
+fi
+
+RESULT=$(echo -e "server $DNSSERVER\nupdate delete $DOMAIN. A\nupdate add $DOMAIN. 300 A $PUBLICIP\nsend" | $NSUPDATE -y hmac-md5:$KEYNAME:$KEY 2>&1)
+if [ $? -ne 0 ]; then
+	echo "Error updating DNS server." >&2
+	echo "Details:" >&2
+	echo "$RESULT" >&2
+	exit 1
+fi
+
+mkdir -p $(dirname $IPFILE)
+echo -n $PUBLICIP >$IPFILE
+exit 0
